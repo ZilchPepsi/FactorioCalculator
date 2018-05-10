8a253c37813abce7b31bb5b14bb7af811a440dbb
caominhim <mcgivern.kevin@live.com>
2018-05-09 18:48:03 -0600

    started implemention of rapidjson.  separted logic from GUI

---
+++ CalculatorInternals/TestMain.cpp
@@ -0,0 +1,17 @@
+#include "JSONInterface.h"
+#include <iostream>
+
+#define WINPAUSE system("pause")
+
+int main()
+{
+	JSONInterface items("testFile.json");
+
+	/*
+	const rapidjson::value& a = (*items.doc)["prototypes"];
+
+	for (rapidjson::sizetype i = 0; i < a.size(); i++)
+		std::cout << a["prototype"].GetString() << std::endl;*/
+
+	return 0;
+}
